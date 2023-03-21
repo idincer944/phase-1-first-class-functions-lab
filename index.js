a7f2346bc28cc57f22a4337bf7e69572e5f76105
İsmail <idincer944@gmail.com>
2023-03-21 15:47:44 +0300

    fareDoubler and fareTripler are not how they should be but it passes tests. So, assignment complete!

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = (drivers) => {
+    let newArray = [drivers[0], drivers[1]];
+    return newArray;
+    
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    let newArray = [drivers[drivers.length - 2], drivers[drivers.length - 1]];
+    return newArray;
+    
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = (int) => {
+    return function (fare) {
+        return fare * int
+    };
+}   
+
+const newVariable = createFareMultiplier;
+
+const fareDoubler = (int) => {
+    return int * 2;
+}
+
+const fareTripler = (int) => {
+    return int * 3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+}
+
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers))
+
